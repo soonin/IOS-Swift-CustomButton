2f8c0b877bf8917d9e75bf93c83e9017902e8393
Pooya Hatami <pooya@node-1w7jr9qrok5tbmfbapha9d96d.ipv6.telus.net>
2018-04-04 05:35:18 -0700

    done! :)

--- IOS-Swift-CustomButton/ViewController.swift
+++ IOS-Swift-CustomButton/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var viewButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        customizeButtonG1(buttonName: viewButton)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func customizeButtonG1(buttonName:UIButton) {
+        // change UIbutton propertie
+        let c1GreenColor = (UIColor(red: -0.108958, green: 0.714926, blue: 0.758113, alpha: 1.0))
+        let c2GreenColor = (UIColor(red: 0.108958, green: 0.714926, blue: 0.758113, alpha: 1.0))
+        buttonName.backgroundColor = UIColor.yellow
+        buttonName.layer.cornerRadius = 7
+        buttonName.layer.borderWidth = 0.8
+        buttonName.layer.borderColor = c1GreenColor.cgColor
+        
+        buttonName.layer.shadowColor = c2GreenColor.cgColor
+        buttonName.layer.shadowOpacity = 0.8
+        buttonName.layer.shadowRadius = 12
+        buttonName.layer.shadowOffset = CGSize(width: 1, height: 1)
+        
+        buttonName.setImage(UIImage(named:"3d-glass-refresh-32X32.png"), for: .normal)
+        buttonName.imageEdgeInsets = UIEdgeInsets(top: 6,left: 100,bottom: 6,right: 14)
+        buttonName.titleEdgeInsets = UIEdgeInsets(top: 0,left: -30,bottom: 0,right: 34)
+        
     }
-
-
+    
 }
 
